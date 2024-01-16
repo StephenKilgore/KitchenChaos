bc3f06a0b8a50e00a4ea118b9681cb4214a3b173
Stephen Kilgore <stephenkilgore07@gmail.com>
2024-01-15 18:55:45 -0600

    plate icon fix

---
+++ Assets/Scripts/PlateIconsSingleUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlateIconSingleUI : MonoBehaviour
+{
+    [SerializeField] private Image image;
+    // Start is called before the first frame update
+    public void SetKitchenObjectSO(KitchenObjectSO kitchenObjectSO)
+    {
+        image.sprite = kitchenObjectSO.sprite;
+    }
+}
