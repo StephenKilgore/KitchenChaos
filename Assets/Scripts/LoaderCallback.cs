b8e594d57d8da7e93fc6ed0e577841bf0615866d
Stephen Kilgore <stephenkilgore07@gmail.com>
2024-01-17 19:17:35 -0600

    Main Menu Done

---
+++ Assets/Scripts/LoaderCallback.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoaderCallback : MonoBehaviour
+{
+   private bool isFirstUpdate = true;
+
+   public void Update()
+   {
+      if (isFirstUpdate)
+      {
+         isFirstUpdate = false;
+         Loader.LoaderCallback();
+      }
+   }
+}
